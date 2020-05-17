7f517ee8046c1e64b1a66ce48cc6b8a3f67654ec
Yisu Rem Wang <remy.sucre@gmail.com>
2020-05-17 10:13:42 -0700

    Transparent title bar
    Make the title bar transparent for a cleaner look. Tested on MacOS.

--- elegance.el
+++ elegance.el
@@ -32,11 +32,15 @@
 (set-face-font 'default "Roboto Mono Light 14")
 (setq default-frame-alist
       (append (list '(width  . 73) '(height . 41)
+                    '(ns-transparent-titlebar . t)
                     '(vertical-scroll-bars . nil)
                     '(internal-border-width . 24)
                     '(font . "Roboto Mono Light 14"))))
 (set-frame-parameter (selected-frame)
                      'internal-border-width 24)
+;; Transparent title bar
+(setq ns-use-proxy-icon nil)
+(setq frame-title-format nil)
 
 ;; Line spacing, can be 0 for code and 1 or 2 for text
 (setq-default line-spacing 0)
